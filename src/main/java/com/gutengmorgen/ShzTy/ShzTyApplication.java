18988ba71d8236a038b313137d56addb7ea333a0
Kevin M <crisstian2345@gmail.com>
2023-09-20 06:29:18 -0500

    Initial commit

---
+++ src/main/java/com/gutengmorgen/ShzTy/ShzTyApplication.java
@@ -0,0 +1,13 @@
+package com.gutengmorgen.ShzTy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShzTyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShzTyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gutengmorgen/ShzTy/ShzTyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gutengmorgen.ShzTy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShzTyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
