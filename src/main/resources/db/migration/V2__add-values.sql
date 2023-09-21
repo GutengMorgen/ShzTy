7dbe21ea3b835fa645a393a601e980a310acd217
Kevin M <crisstian2345@gmail.com>
2023-09-20 21:07:00 -0500

    agrega valores predeterminados a las tablas genres, languages, playlists y album_formats

---
+++ src/main/resources/db/migration/V2__add-values.sql
@@ -0,0 +1,10 @@
+INSERT genres(name) VALUES('Rock'),('Pop'),('Hip-Hop'),('Jazz'),('Metal'),('Classical'),('Other');
+INSERT languages(name) VALUES('English'),('Spanish'),('French'),('German'),('Italian'),('Japanese'),('Chinese'),('Korean'),('Portuguese'),('Russian'),('Other');
+INSERT playLists(tile) VALUES('Addictive'),('TasteLess'),('Nonchalant'),('Common'),('Unacceptable');
+INSERT album_formats(name) VALUES('CD'),('LP'),('DOUBLE LP'),('EP'),('SINGLE'),('MIX TAPE'),('VINYL'),('CASSETTE'),('DEMO'),('OTHER');
+
+-- INSERT artists(name, born_date, gender, country, biography, id_languages, id_genres) VALUES('The Beatles', '1960-07-08', 'Male', 'United Kingdom', 'British rock band formed in Liverpool in 1960.', 1, 1);
+-- INSERT artists(name, born_date, gender, country, biography, id_languages, id_genres) VALUES('The Rolling Stones', '1960-07-08', 'Male', 'United Kingdom', 'British rock band formed in Liverpool in 1960.', 1, 1);
+-- INSERT artists(name, born_date, gender, country, biography, id_languages, id_genres) VALUES('The Who', '1960-07-08', 'Male', 'United Kingdom', 'British rock band formed in Liverpool in 1960.', 1, 1);
+-- INSERT artists(name, born_date, gender, country, biography, id_languages, id_genres) VALUES('The Doors', '1960-07-08', 'Male', 'United Kingdom', 'British rock band formed in Liverpool in 1960.', 1, 1);
+-- INSERT artists(name, born_date, gender, country, biography, id_languages, id_genres) VALUES('The Beatles', '1960-07-08', 'Male', 'United Kingdom', 'British rock band formed in Liverpool in 1960.', 1, 1);
