3adfd68302711c4c3b554dc96901606fe1d2b4bf
Kevin M <crisstian2345@gmail.com>
2023-09-21 23:48:13 -0500

    agrega mas dtos

--- src/main/java/com/gutengmorgen/ShzTy/Controllers/ArtistController.java
+++ src/main/java/com/gutengmorgen/ShzTy/Controllers/ArtistController.java
@@ -1,4 +1,41 @@
 package com.gutengmorgen.ShzTy.Controllers;
 
+import com.gutengmorgen.ShzTy.Repositories.ArtistRepo;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/shzty/artists")
 public class ArtistController {
+
+    private final ArtistRepo repository;
+
+    public ArtistController(ArtistRepo repository) {
+        this.repository = repository;
+    }
+
+
+    @GetMapping
+    public String list() {
+        return "Artists";
+    }
+
+    @GetMapping(path = "/{id}")
+    public String findById() {
+        return "Artist";
+    }
+
+    @PostMapping
+    public String create() {
+        return "Artist";
+    }
+
+    @PutMapping(path = "/{id}")
+    public String update() {
+        return "Artist";
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String delete() {
+        return "Artist";
+    }
 }
--- src/main/java/com/gutengmorgen/ShzTy/Entities/Artists/Artist.java
+++ src/main/java/com/gutengmorgen/ShzTy/Entities/Artists/Artist.java
@@ -1,4 +1,39 @@
 package com.gutengmorgen.ShzTy.Entities.Artists;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.gutengmorgen.ShzTy.Entities.Genres.Genre;
+import com.gutengmorgen.ShzTy.Entities.Languages.Language;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "artists")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Artist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    private String Name;
+    private Date BornDate;
+    private String Gender;
+    private String Country;
+    private String biography;
+
+    @ManyToMany
+    @JoinColumn(name = "language_id")
+    @JsonBackReference
+    private Language language;
+
+    @ManyToMany
+    @JoinColumn(name = "genre_id")
+    @JsonBackReference
+    private Genre genre;
 }
